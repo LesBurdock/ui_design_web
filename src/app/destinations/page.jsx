88c4bbe7e719030ae1a1e54be557eea3b155994f
LesBurdock <burdocklesley@gmail.com>
2025-07-02 13:56:19 +0200

    added responsive sizing and layout destination page

--- src/app/destinations/page.jsx
+++ src/app/destinations/page.jsx
@@ -11,61 +11,84 @@ export default function DestinationsPage() {
   const destinations = data.destinations;
   const activeDestination = destinations[activeIndex];
   return (
-    
     <div className="p-4">
       <NavButton />
-      <main className={`${styles.grid_container} ${styles.grid_container__home}`}>
-    {/* Top-left: Page header */}
-    <div className={styles.grid_header}>
-      <h1 className={`${styles.numbered_title}`}><span>01</span> PICK YOUR DESTINATION</h1>
-    </div>
-      {/* Tab Buttons */}
-      <div  role="tablist"
-                className={`${styles.underline_indicators} ${styles.flex} ${styles.tab_group}`}>
-        {destinations.map((destination, index) => (
-         <button
-         key={index}
-         onClick={() => setActiveIndex(index)}
-         role="tab"
-         aria-selected={index === activeIndex ? "true" : "false"}
-         className={`
+      <main
+        className={`flow ${styles.grid_container} ${styles.grid_container__destination}`}
+        style={{ "--flow_space": "2rem" }}
+      >
+        {/* Top-left: Page header */}
+          <h1 className={`${styles.numbered_title}`}>
+            <span>01</span> PICK YOUR DESTINATION
+          </h1>
+        {/* Tab Buttons */}
+        <div
+          role="tablist"
+          className={`${styles.underline_indicators} ${styles.flex} ${styles.tab_group}`}
+        >
+          {destinations.map((destination, index) => (
+            <button
+              key={index}
+              onClick={() => setActiveIndex(index)}
+              role="tab"
+              aria-selected={index === activeIndex ? "true" : "false"}
+              className={`
            ${styles.fgLight} 
            ${styles.bgDark} 
            ${styles.sansCond} 
            ${styles.uppercase} 
            ${styles.letter_spacing_2} 
-           ${index === activeIndex ? styles.active_tab : ''}
+           ${index === activeIndex ? styles.active_tab : ""}
          `}
-       >
-         {destination.name}
-       </button>
-        ))}
-      </div>
-
-      {/* Destination Content */}
-      <div>
-        <div>
-          <h2 className={`${styles.fs_900} ${styles.uppercase} ${styles.fgWhite} ${styles.serif} ${styles.d_block}`}>
-            {activeDestination.name}
-          </h2>
-          <p>{activeDestination.description}</p>
-          <p className={`${styles.fgLight} ${styles.serif}`}>
-            Distance: {activeDestination.distance}
-          </p>
-          <p>
-            Travel Time: {activeDestination.travel}
-          </p>
-          <div className={styles.destination_image}>
-            <Image
-              src={activeDestination.images.png.replace('./assets', '/assets')}
-              alt={activeDestination.name}
-              width={200}
-              height={200}
-            />
-          </div>
+            >
+              {destination.name}
+            </button>
+          ))}
         </div>
-      </div>
-    </main>
+
+        {/* Destination Content */}
+<div className={styles.destination_image_wrapper}>
+          <Image
+          className={styles.destination_image}
+            src={activeDestination.images.png.replace("./assets", "/assets")}
+            alt={activeDestination.name}
+            fill
+             sizes="(max-width: 600px) 100vw, 400px"
+            style={{ objectFit: "contain" }}
+            priority
+          />
+          </div>
+        <article className={`${styles.destination_info}`}>
+          <h3
+            className={`${styles.fs_800} ${styles.uppercase} ${styles.fgWhite} ${styles.serif} ${styles.d_block}`}
+          >
+            {activeDestination.name}
+          </h3>
+          <p>{activeDestination.description}</p>
+          <div className={`${styles.flex} ${styles.destination_meta}`}>
+            <div>
+              <h2
+                className={`${styles.fgLight} ${styles.uppercase} ${styles.fs_200}`}
+              >
+                Avg. Distance:
+              </h2>
+              <p className={`${styles.fgWhite} ${styles.fs_500}`}>
+                {activeDestination.distance}
+              </p>
+            </div>
+            <div>
+              <h2
+                className={`${styles.fgLight} ${styles.uppercase} ${styles.fs_200}`}
+              >
+                Est. Travel Time:{" "}
+              </h2>
+              <p className={`${styles.fgWhite} ${styles.fs_500}`}>
+                {activeDestination.travel}
+              </p>
+            </div>
+          </div>
+        </article>
+      </main>
     </div>
   );
 }
